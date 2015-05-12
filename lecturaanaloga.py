7da2a022e064a35ec4f8c625d7117c39ffc53954
rudolph1991 <rudolph.fire@gmail.com>
2015-05-12 18:32:04 -0500

    Create lecturaanaloga.py

---
+++ lecturaanaloga.py
@@ -0,0 +1,17 @@
+#lectura de datos por canal 0
+#pcf pcf8591 direccion 0x48
+import time
+import smbus
+
+bus = smbus.SMBus (1)
+
+print ("Read the A/D")
+print ("crtl c to stop")
+bus.write_byte(0x48, 0x40) #dispositivo y canal analogo
+
+while True: #do forever
+        reading = bus.read_byte(0x48) #read A/D
+        voltaje = reading * (3.3/255)
+        pH = voltaje * (14.0/3.2)
+        print (pH)
+        time.sleep(1)
